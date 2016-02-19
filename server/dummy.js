41366848e3691c4553d7b1f5fa0ec13091b6c2ff
Tim Kettering <tim@blackcore.com>
2016-02-19 12:46:52 -0800

    updating dummy server to rotate through three demo json files.

--- server/dummy.js
+++ server/dummy.js
@@ -1,28 +1,37 @@
 // constants
-var KINECT_DATA_FILE = './dummy-data/skeletal-output-01.json';
+var KINECT_DATA_FILES = ['./dummy-data/skeletal-output-01.json', './dummy-data/skeletal-output-02.json', './dummy-data/skeletal-stress-test.json'];
 var LISTEN_PORT = '8008';
 
 var io = require('socket.io')(LISTEN_PORT);
 var json = require('json-file');
 var term = require('terminal-kit').terminal;
-var output = json.read(KINECT_DATA_FILE);
-var array = output.get('bodiesData');
 var frameCounter = 0;
+var fileCounter = 0;
+var workingFile = json.read(KINECT_DATA_FILES[fileCounter]);
+var array = workingFile.get('bodiesData');
 
 term.nextLine();
 term.bold.yellow("Dummy Kinect Server started.  Listening for connections on port: " + LISTEN_PORT).nextLine();
 
 io.on('connection', function(socket){
 
-	term.colorGrayscale(200)("Connection established, serving dummy data: '" + KINECT_DATA_FILE + "'").nextLine();
+	term.colorGrayscale(200)("Connection established, serving dummy data.").nextLine();
+
 	var counter = 0;
 
 	var intervalID = setInterval(function() {
-		socket.emit("bodyFrame", array[counter]);
 
+		socket.emit("bodyFrame", array[counter]);
 		counter++;
+
 		if(counter >= array.length) {
+			// reset counter.
 			counter = 0;
+
+			// advance to next file
+			fileCounter++;
+			workingFile = json.read(KINECT_DATA_FILES[fileCounter % KINECT_DATA_FILES.length]);
+			array = workingFile.get('bodiesData');
 		}
 
 		frameCounter++;
